a640c32cdaf7277f6d2f26f10705ae3fc6d9e5d5
Lawrence A. Damba <dambapls@gmail.com>
2019-10-23 22:54:06 -0500

    Changes to be committed: 	new file:   init.el

---
+++ init.el
@@ -0,0 +1,52 @@
+(require 'package)
+(setq package-enable-at-startup nil)
+(add-to-list 'package-archives
+	     '("melpa" . "https://melpa.org/packages/"))
+
+(package-initialize)
+
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+
+
+(use-package evil
+  :ensure t
+  :init
+  (evil-mode 1))
+
+
+(use-package which-key
+  :ensure t
+  :init
+  (which-key-mode))
+
+
+(use-package org
+  :ensure t
+  :init
+  (org-mode))
+
+(tool-bar-mode 1)
+(menu-bar-mode 1)
+(scroll-bar-mode -1)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes (quote (solarized-dark)))
+ '(custom-safe-themes
+   (quote
+    ("d91ef4e714f05fff2070da7ca452980999f5361209e679ee988e3c432df24347" "0598c6a29e13e7112cfbc2f523e31927ab7dce56ebb2016b567e1eff6dc1fd4f" "a8245b7cc985a0610d71f9852e9f2767ad1b852c2bdea6f4aadc12cce9c4d6d0" default)))
+ '(line-number-mode nil)
+ '(package-selected-packages (quote (which-key use-package)))
+ '(show-paren-mode t))
+ 
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:inherit nil :stipple nil :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 98 :width normal :foundry "monotype" :family "Cousine")))))
